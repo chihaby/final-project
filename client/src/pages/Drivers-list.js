7fb826f710768a8ce4a34ac41e96871b85ccec06
MARIS <marisangelique@gmail.com>
2019-05-16 19:39:53 -0700

    Map

--- client/src/pages/Drivers-list.js
+++ client/src/pages/Drivers-list.js
@@ -6,6 +6,12 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import Consumer from "../context/configContext";
+<<<<<<< Updated upstream
+=======
+import faker from "faker";
+import SimpleMap from "../components/Map/index.js";
+
+>>>>>>> Stashed changes
 
 class DriversList extends Component {
     state = {
@@ -55,46 +61,68 @@ class DriversList extends Component {
 
     render() {
         return (
-        <Consumer>
-            {(context) => {
-            const { drivers } = context.library || [];
-            return(
-            <Container fluid>
-                <Row>
-                    <Col size="md-6-offset-3 sm-12 lg-12">
-                        <Jumbotron>
-                        <h1>Drivers List</h1>
-                        </Jumbotron>
-                        {this.state.drivers.length ? (
-                        <List>
-                            {this.state.drivers.map(driver => (
-                            <ListItem key={driver._id}>
-                                <button className="like" onClick={()=> context.library.incrementLikes(driver._id)}><i className="fas fa-thumbs-up"></i>
-                                <span className="like-count">{(drivers.find(search => search.id === driver._id)) ? drivers.find(search => search.id === driver._id).likes : 0}</span>
-                                </button>
-                                <Link to={"/driversList/" + driver._id}>
-                                <strong>
-                                    {driver.firstName} - {driver.lastName}
-                                </strong>
-                                </Link>
-                                <DeleteBtn onClick={() => this.deleteDriver(driver._id)} />
-                            </ListItem>
-                            ))}
-                        </List>
-                        ) : (
-                        <h3>No Results to Display</h3>
-                        )}
-                    </Col>
-                </Row>
-                <Row>
-                <Col size="md-2">
-                    <Link to="/">← Home</Link>
-                </Col>
-                </Row>
-            </Container>
-            )}
-            }
-        </Consumer>
+            <Consumer>
+                {(context) => {
+                    const { drivers } = context.library || [];
+                    return (
+                        <Container fluid>
+                            <Row>
+                                <Col size="md-6-offset-3 sm-12 lg-12">
+                                    <Jumbotron>
+                                        <h1>Drivers List</h1>
+                                    </Jumbotron>
+                                    <Row>
+                                        <SimpleMap/>
+                                        {this.state.drivers.length ? (
+                                            <List>
+                                                {this.state.drivers.map(driver => (
+                                                    <ListItem key={driver._id}>
+                                                        <button className="like" onClick={() => context.library.incrementLikes(driver._id)}>
+                                                            <i className="fas fa-thumbs-up">&nbsp;&nbsp;</i>
+                                                            <span className="like-count">{(drivers.find(search => search.id === driver._id)) ? drivers.find(search => search.id === driver._id).likes : 0}</span>
+                                                        </button>
+                                                        <Link to={"/driversList/" + driver._id}>
+                                                            <strong>
+                                                                {driver.firstName} {' '} {driver.lastName} <br />
+                                                            </strong>
+                                                        </Link>
+                                                        <img src={faker.image.avatar()} alt={"img"} width="50" height="50" /> <br />
+                                                        <span className="destination">Destination: </span>{driver.destination} <br />
+                                                        <span className="destination">From: </span> <br />
+                                                        <span className="destination">Time: </span>
+                                                        <DeleteBtn onClick={() => this.deleteDriver(driver._id)} />
+                                                    </ListItem>
+                                                ))}
+                                            </List>
+                                        ) : (
+                                                <h3>No Results to Display</h3>
+                                            )}
+                                    </Row>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <p>Temporary Links</p> <br />
+                                <Col size="md-2">
+                                    <Link to="/">← Home</Link>
+                                </Col>
+                                <Col size="md-2">
+                                    <Link to="/drivers">← Drivers-Form</Link>
+                                </Col>
+                                <Col size="md-2">
+                                    <Link to="/riders">← Riders-Form</Link>
+                                </Col>
+                                <Col size="md-2">
+                                    <Link to="/driversList">← Drivers-List</Link>
+                                </Col>
+                                <Col size="md-2">
+                                    <Link to="/ridersList">← Riders-List</Link>
+                                </Col>
+                            </Row>
+                        </Container>
+                    )
+                }
+                }
+            </Consumer>
         );
     }
 }
--- client/src/pages/Riders-list.js
+++ client/src/pages/Riders-list.js
@@ -6,6 +6,11 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 import Consumer from "../context/configContext";
+<<<<<<< Updated upstream
+=======
+import faker from "faker";
+import SimpleMap from "../components/Map/index.js";
+>>>>>>> Stashed changes
 
 class RidersList extends Component {
     state = {
@@ -57,6 +62,7 @@ class RidersList extends Component {
         return (
             <Consumer>
                 {(context) => {
+<<<<<<< Updated upstream
                 const { riders } = context.ridersLibrary || [];
                 return (
                 <Container fluid>
@@ -93,6 +99,66 @@ class RidersList extends Component {
                     </Row>
                 </Container>
                 )}
+=======
+                    const { riders } = context.ridersLibrary || [];
+                    return (
+                        <Container fluid>
+                            <Row>
+                                <Col size="md-3-offset-3 sm-6 lg-12">
+                                    <Jumbotron>
+                                        <h1>Riders List</h1>
+                                    </Jumbotron>
+                                    <Row>
+                                        <SimpleMap/>
+                                        {this.state.riders.length ? (
+                                            <List>
+                                                {this.state.riders.map(rider => (
+                                                    <ListItem key={rider._id}>
+                                                        <button className="like" onClick={() => context.ridersLibrary.incrementLikes(rider._id)}>
+                                                            <i className="fas fa-thumbs-up">&nbsp;&nbsp;</i>
+                                                            <span className="like-count">{(riders.find(search => search.id === rider._id)) ? riders.find(search => search.id === rider._id).likes : 0}</span>
+                                                        </button>
+                                                        <Link to={"/ridersList/" + rider._id}>
+                                                            <strong>
+                                                                {rider.firstName} {' '} {rider.lastName} <br />
+                                                            </strong>
+                                                        </Link>
+                                                        <img src={faker.image.avatar()} alt={"img"} width="50" height="50" /> <br />
+                                                        <span className="destination">Destination: </span>{rider.destination} <br />
+                                                        <span className="destination">From: </span> <br />
+                                                        <span className="destination">Time: </span>
+                                                        <DeleteBtn onClick={() => this.deleteRider(rider._id)} />
+                                                    </ListItem>
+                                                ))}
+                                            </List>
+                                    ) : (
+                                            <h3>No Results to Display</h3>
+                                        )}
+                                    </Row>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <p>Temporary Links</p> <br />
+                                <Col size="md-2">
+                                    <Link to="/">← Home</Link>
+                                </Col>
+                                <Col size="md-2">
+                                    <Link to="/drivers">← Drivers-Form</Link>
+                                </Col>
+                                <Col size="md-2">
+                                    <Link to="/riders">← Riders-Form</Link>
+                                </Col>
+                                <Col size="md-2">
+                                    <Link to="/driversList">← Drivers-List</Link>
+                                </Col>
+                                <Col size="md-2">
+                                    <Link to="/ridersList">← Riders-List</Link>
+                                </Col>
+                            </Row>
+                        </Container>
+                    )
+                }
+>>>>>>> Stashed changes
                 }
             </Consumer>
         );
