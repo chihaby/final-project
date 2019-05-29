b2098c0629e914798151f47aee47468aba616162
Rad <chihaby@gmail.com>
2019-05-28 19:26:03 -0700

    working v

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,16 +1,16 @@
-// import React, { Component } from "react";
-// import Message from "../components/Message";
-// import SendMessageForm from "../components/MessageFormSend";
-// import MessageList from "../components/MessageList";
+import React, { Component } from "react";
+import Message from "../components/Message";
+import SendMessageForm from "../components/MessageFormSend";
+import MessageList from "../components/MessageList";
 
-// class Chat extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Message />
-//             </div>
-//         );
-//     }
-// }
+class Chat extends Component {
+    render() {
+        return (
+            <div>
+                <Message />
+            </div>
+        );
+    }
+}
 
-// export default Chat;
+export default Chat;
